3afcd20496578e3a770afc0aadcacc060ff4f4e9
aswan aswan <aaswan048@gmail.com>
2022-12-31 17:27:43 +0000

    last commit

--- scripts/listeners.js
+++ scripts/listeners.js
@@ -2,48 +2,88 @@
 import {getWord , checkWord} from "./words.js"
 
 let secret  = [...getWord().toUpperCase()] ;
-console.log(secret);
+// let this console work for win testing only ;
+console.log(secret) ;
 // generate  a  word
-
+let styleIndex  = 0 ;
+if(localStorage.getItem("styleIndex") == null){
+    localStorage.setItem("styleIndex" , styleIndex.toString()) ;
+}else{
+    styleIndex = localStorage.getItem("styleIndex") ;
+}
 
 export const magic = (word) => {
     
     // start working  with  words module
     let obj = {}
     word = [...word.toUpperCase()]
-    console.log(word);
+    // console.log(word);
 
     // be careful with beautiful for loop 
     word.forEach((ele , index) => {
         if(secret.includes(ele) === false){
-            obj[index] = "red"
+            obj[index] = "wrong"
         }else{
-            obj[index] = "yellow"
+            obj[index] = "maybe"
         }
         if(ele == secret[index]){
-            obj[index] = "green"
+            obj[index] = "confirm"
         }
     })
 
-    console.log(obj)
+    // console.log(obj)
     let inputs = document.querySelector(".ready").querySelectorAll("input")
     inputs.forEach((ele , index) => {
-        ele.style.backgroundColor = obj[index] ;
+        ele.classList.add(obj[index]) ;
+        let op = word[index] ;
+        // console.log(op)
+        let key = document.querySelector(".keyboard").querySelectorAll(`li[value="${op}"]`)[0] ;
+        if(obj[index] =="wrong"){
+            key.style.border = "none" ;
+        }else{
+            key.classList.add(obj[index]) ;
+        }
+        
+        
     })
+
+
 
     // change the form class to verifiy that they are not yet targeted to fill inputs
     document.querySelector('.ready').classList.replace("ready","verified");
     
     // if there is not more word to fill stop / all green mean game over
     let gameWin = Object.values(obj)
-    let count = gameWin.filter(x => x=="green").length
-    console.log(count)
+    let count = gameWin.filter(x => x=="confirm").length
+    
     
 
-    if (document.querySelector('form[class ="word"]') != null && count < 5 ){
-        document.querySelectorAll('form[class ="word"]')[0].classList.add('working') ;
+    // if  count  is  5  mean  you win  , 
+    if(count == 5 ){
+        // console.log("win")
+        let x = document.querySelectorAll('form[class ="word"]') ;
+        x.forEach((ele) => {
+            ele.classList.replace("word" , "gameover") ;
+        })
+        // say you will 
+        let notifyme = document.querySelector(".notification") ;
+        notifyme.textContent = "Congratulations , you won!" ;
+        notifyme.style.display = "block"
+        setTimeout(()=>{
+            notifyme.style.display = "none"
+        } , 10000)
+
     }else{
-        console.log('game over')
+        if (document.querySelector('form[class ="word"]') != null && count != 5 ){
+        document.querySelectorAll('form[class ="word"]')[0].classList.add('working') ;
+        }else{
+            let notifyme = document.querySelector(".notification") ;
+            notifyme.textContent = "Good luck next time" ;
+            notifyme.style.display = "block"
+            setTimeout(()=>{
+                notifyme.style.display = "none"
+            } , 3000)
+        }
     }
 
 }
@@ -56,8 +96,18 @@ export const getVerified= () => {
     })
 
     if(checkWord(word.toLocaleLowerCase()) == true){
-        console.log(word , "exist")
+        // console.log(word , "exist")
         magic(word) ;
+    }else{
+        let notifyme = document.querySelector(".notification") ;
+        notifyme.textContent = "Please write correct word" ;
+        notifyme.style.display = "block"
+        setTimeout(()=>{
+            notifyme.style.display = "none"
+        } , 3000)
+
+
+        
     }
 }
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,44 @@
 const alphabet = [ 'q' , 'w' , 'e' , 'r' ,'t' , 'y' , 'u' , 'o' ,'p' , 'i' , 'a' , 's' ,'d', 'f' , 'g' , 'h','j' , 'k' , 'l' , 'z' ,'x' , 'c' , 'v' , 'b' ,'Del' ,'n' , 'm'  , 'Enter' ]
 
+const colors = [
+    {
+        bg: '#8b966e',
+        light: '#c4d0a3',
+        dark: '#1f1f1f',
+        accent: '#5d6942',
+    },
+    {
+        bg: '#966d8c',
+        light: '#d1a3bd',
+        dark: '#210317',
+        accent: '#773863',
+    },
+    {
+        bg: '#5e7a9e',
+        light: '#a3b3d1',
+        dark: '#020a21',
+        accent: '#0052bf',
+    },
+    {
+        bg: '#9e775d',
+        light: '#e2c29c',
+        dark: '#211201',
+        accent: '#bf6200',
+    },
+    ];
+// local  storage  for  the  themes
+let styleIndex  = 0 ;
+if(localStorage.getItem("styleIndex") == null){
+    localStorage.setItem("styleIndex" , styleIndex.toString()) ;
+}else{
+    styleIndex = localStorage.getItem("styleIndex") ;
+}
+
+
+
+
+
+
 // insert 25 inputs
 let i = 0 ;
 while (i < 5){
@@ -22,7 +61,7 @@ let  keyboard = document.createElement("ul") ;
 keyboard.className = "keyboard" ;
 let  content = ""
 alphabet.forEach((element , index) => {
-    let string = `<li class="key" id="${index}">${element.toUpperCase()}</li>` ;
+    let string = `<li class="key" value = "${element.toUpperCase()}" id="${index}">${element.toUpperCase()}</li>` ;
     content+= string ;
 }) ;
 keyboard.innerHTML = content ;
@@ -49,4 +88,64 @@ document.querySelector('.home').addEventListener("click" , ()=> {
     document.querySelector(".details").style.display = "none" ;
     document.querySelector(".starter").style.display = "flex" ;
     document.querySelector(".home").style.display = "none" ;
+})
+
+
+// themes container 
+
+
+let j = 0 ;
+while (j < 4){
+    let themes = document.querySelector('.colors') ;
+    let mystyle  = document.createElement('div') ;
+    mystyle.className = "mystyle" ;
+    mystyle.innerHTML = `
+    <div></div><p> background color </p>
+    <div></div><p>light color</p>
+    <div></div><p>dark color</p>
+    <div></div><p>accent color</p>`
+    mystyle.setAttribute("index" , j);
+    themes.appendChild(mystyle) ;
+    let styles = document.querySelectorAll('.mystyle')[j].querySelectorAll('div') ;
+    let k = 0 ;
+    let s = ['bg' ,'light','dark' ,'accent']
+    styles.forEach((div) => {
+        let nm = s[k];
+        div.style.backgroundColor = colors[j][nm] ;
+        k++
+    })
+    j++ ;
+}
+
+
+
+document.querySelector('.theme').addEventListener("click" , ()=> {
+    document.querySelector(".themes").style.display = "flex" ;
+})
+
+document.querySelector('.close').addEventListener("click" , ()=> {
+    document.querySelector(".themes").style.display = "none" ;
+})
+
+// local  storage  for  the  themes 
+// default variables 
+let root = document.querySelector(":root")
+root.style.setProperty("--background" , colors[styleIndex]['bg'])
+root.style.setProperty("--light" , colors[styleIndex]['light'])
+root.style.setProperty("--dark" , colors[styleIndex]['dark'])
+root.style.setProperty("--accent" , colors[styleIndex]['accent'])
+
+
+
+
+// changing  default colors
+document.querySelector('.themes').addEventListener("click" , (e)=> {
+    if(e.target.className =="mystyle"){
+        let  index  =  e.target.getAttribute("index") ;
+        localStorage.setItem("styleIndex" , index) ;
+        root.style.setProperty("--background" , colors[index]['bg'])
+        root.style.setProperty("--light" , colors[index]['light'])
+        root.style.setProperty("--dark" , colors[index]['dark'])
+        root.style.setProperty("--accent" , colors[index]['accent'])
+    }
 })
